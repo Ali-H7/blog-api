b8a0e2bf2d1056bc85bfc5d55cf95cf39a797998
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-02-11 11:32:39 +0300

    Add services to find posts

--- services/post.service.js
+++ services/post.service.js
@@ -1,13 +1,14 @@
 import prisma from '../config/prisma';
 
 async function createPost(postObject) {
-  const { title, content, dateCreated, published, userId } = postObject;
+  const { title, content, dateCreated, published, slug, userId } = postObject;
   await prisma.post.create({
     data: {
       title,
       content,
       dateCreated,
       published,
+      slug,
       user: {
         connect: {
           id: userId,
@@ -17,4 +18,27 @@ async function createPost(postObject) {
   });
 }
 
-export default { createPost };
+async function findPost(slug) {
+  const post = await prisma.post.findUnique({
+    where: {
+      slug,
+    },
+  });
+  return post;
+}
+
+async function findPublishedPosts() {
+  const publishedPosts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+  });
+  return publishedPosts;
+}
+
+async function findAllPosts() {
+  const posts = await prisma.post.findMany();
+  return posts;
+}
+
+export default { createPost, findPost, findPublishedPosts, findAllPosts };
