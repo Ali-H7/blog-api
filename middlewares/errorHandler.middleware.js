1dcdf1754aae5ed9654fdfb5d4b4e48a4c20da4a
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-02-15 15:26:48 +0300

    Refactor error handler

--- middlewares/errorHandler.middleware.js
+++ middlewares/errorHandler.middleware.js
@@ -1,11 +1,29 @@
 import 'dotenv/config';
+import camelCase from 'camelcase';
+import { createError } from '../helpers/index.js';
+import { Prisma } from '../generated/prisma/index.js';
 
-function errorHandler(err, _req, res, _next) {
+function handleDatabaseErrors(err, _req, res, next) {
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === 'P2002') {
+      const errorMessages = {
+        tag: 'There already a tag with this name',
+        generic: 'Duplicate entry: this value already exists in the database.',
+      };
+      const model = camelCase(err.meta.modelName);
+      const message = errorMessages[model] || errorMessages.generic;
+      const newErr = createError(message, 409);
+      next(newErr);
+    }
+  }
+  next(err);
+}
+
+function finalErrorHandler(err, _req, res, _next) {
   const statusCode = err.statusCode || 500;
-  const errorMessage = statusCode >= 500 ? 'Internal Server Error' : err.message;
   const errorStack = process.env.NODE_ENV === 'development' ? err.stack : null;
-  console.error(err);
+  const errorMessage = statusCode >= 500 ? 'Internal Server Error' : err.message;
   res.status(statusCode).json({ error: errorMessage, stack: errorStack });
 }
 
-export default errorHandler;
+export default [handleDatabaseErrors, finalErrorHandler];
